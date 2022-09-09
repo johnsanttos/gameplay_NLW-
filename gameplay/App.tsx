6a43d6040b51b9df9c9dcf5121d24637e23fdf4a
John <jhow.mito11@gmail.com>
2022-09-09 12:11:29 -0300

    starting the login page

---
+++ gameplay/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import SignIn from './src/screens/SignIn';
+
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+
+   <SignIn/>
+
+  );
+}
