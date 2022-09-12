ed311e554eeed2fe4f3df7f406bae5fd266e8d8a
John <jhow.mito11@gmail.com>
2022-09-12 11:53:31 -0300

    gradients background effect

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -3,10 +3,10 @@ import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular,Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium , Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { StatusBar} from 'react-native';
 
+import { Background } from './src/components/Background';
 import {SignIn} from './src/screens/SignIn';
-
-import { StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [fontLoaded] = useFonts ({
@@ -21,13 +21,13 @@ return <AppLoading/>
   }
 
   return (
-    <>
+    <Background>
     <StatusBar
     barStyle='light-content'
     backgroundColor='transparent'
     translucent
 />
    <SignIn/>
-   </>
+   </Background>
   );
 }
