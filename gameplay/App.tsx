c7befc532d1decd82ee889a3f3f4dc71e9e8140e
John <jhow.mito11@gmail.com>
2022-09-12 14:54:25 -0300

    starting home screen and profile

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular,Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium , Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import { StatusBar} from 'react-native';
+import { Home } from './src/screens/Home';
 
 import { Background } from './src/components/Background';
 import {SignIn} from './src/screens/SignIn';
@@ -27,7 +28,7 @@ return <AppLoading/>
     backgroundColor='transparent'
     translucent
 />
-   <SignIn/>
+   <Home/>
    </Background>
   );
 }
--- gameplay/src/components/Background/index.tsx
+++ gameplay/src/components/Background/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { styles } from "./style";
+import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 
 type Props = {
