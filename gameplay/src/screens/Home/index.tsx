4f7f90566d2cfbd35fc6fc447126216581dfa6f5
John <jhow.mito11@gmail.com>
2022-09-14 16:27:21 -0300

    Component Appointments

--- gameplay/src/screens/Home/index.tsx
+++ gameplay/src/screens/Home/index.tsx
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { View ,Text, FlatList } from 'react-native'
+
 import { ButtonAdd } from '../../components/ButtonAdd'
 import { CategorySelected } from '../../components/CategorySelected'
+import { ListHeader } from '../../components/ListHeader'
 import { Profile } from '../../components/Profile'
 import { styles } from './style'
 
 export function Home() {
     const [category,setCategory] = useState('');
 
+    const appointments =[
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date:'14/09 ás 15:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma parrtida da Midware'
+        }
+    ]
+
 function handleCategorySelected(categoryId:string) {
+
      categoryId === category ? setCategory ('')  : setCategory(categoryId)
 }   
  return (
@@ -22,9 +40,24 @@ function handleCategorySelected(categoryId:string) {
 
                 <CategorySelected 
                 categorySelected={category}
-                setCategory= {handleCategorySelected}
-              
+                setCategory= {handleCategorySelected}          
                 />
+                <View style={styles.content} >
+                    
+                    <ListHeader
+                    title='Partidas agendadas'
+                    subtitle='Total 6'
+                    />
+
+                    <FlatList
+                    data={appointments}
+                    keyExtractor ={item => item.id}
+                    renderItem ={({item}) => (
+                        
+                    ) }
+                    />
+
+                </View>
             </View>
         </View>
     )
