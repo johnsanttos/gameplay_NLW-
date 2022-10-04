bde68d753770cdc13f14a9351ab4e25de7181551
John Santos <jhow.mito11@gmail.com>
2022-10-04 17:33:01 -0300

    Component TextArea

--- gameplay/src/screens/AppointmentCreate/index.tsx
+++ gameplay/src/screens/AppointmentCreate/index.tsx
@@ -1,27 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
 } from 'react-native';
+import {Feather} from '@expo/vector-icons'
 import { styles } from './styles';
 import { Header } from '../../components/Header';
 import { Background } from '../../components/Background';
-
+import { GuildIcon } from '../../components/GuildIcon';
 import { theme } from '../../global/styles/theme';
+import { CategorySelected } from '../../components/CategorySelected';
+import { RectButton } from 'react-native-gesture-handler';
+import { SmallInput } from '../../components/SmallInput';
+import { TextArea } from '../../components/TextArea';
 
 
 
 export function AppointmentCreate() {
 
+  const [category,setCategory] = useState('');
+
+
   return (
     <Background>
       <Header
-        title='Agendar partida'   
+        title='Agendar partida'
       />
-    <Text  style ={styles.label}>
-      Categoria
-    </Text>
+      <Text style={[styles.label,
+        {marginLeft:24, marginTop: 36, marginBottom:18} ]}>
+        Categoria
+      </Text>
 
+      <CategorySelected
+        hasCheckBox
+        setCategory={setCategory}
+        categorySelected={category}
+      />
+
+      <View style={styles.form}>
+      <RectButton>
+        <View style={styles.select}> 
+
+     {   
+   
+        <GuildIcon/>
+        }
+
+        <View style={styles.selectBody} >
+          <Text style={styles.label} > 
+          Selecione um servidor
+          </Text>
+        </View>
+        <Feather
+        name= "chevron-right"
+        color = {theme.colors.heading}
+        size= {18}
+        />
+        </View>
+      </RectButton>
+
+
+    
+      
+
+      <View style={styles.field}>
+        <View>
+          <Text style={styles.label} >
+            Dia e mês
+          </Text>
+
+          <View style={styles.column}>
+          <SmallInput maxLength={2} />
+          <Text style={styles.divider}>
+            /
+          </Text>
+          <SmallInput maxLength={2} />
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.label} >
+           Hora e minuto
+          </Text>
+
+          <View style={styles.column}>
+          <SmallInput maxLength={2} />
+          <Text style={styles.divider}>
+            :
+          </Text>
+          <SmallInput maxLength={2} />
+          </View>
+
+          
+        </View>
+
+        
+      </View>
+      <TextArea
+      multiline
+      maxLength={100}
+      numberOfLines ={5}
+      autoCorrect ={false}
+      />
+      </View>
     </Background>
   )
 }
