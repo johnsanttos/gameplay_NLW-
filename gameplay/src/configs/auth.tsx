07f6011e75a0db409033bc8f0bf913a3bda7468d
John Santos <jhow.mito11@gmail.com>
2022-10-27 15:55:53 -0300

    Context Api p2

--- gameplay/src/configs/auth.tsx
+++ gameplay/src/configs/auth.tsx
@@ -1,5 +1,9 @@
-const REDIRECT_URI =''
-const SCOPE ='';
-const RESPONSE_TYPE =''
-const CLIENT_ID = '';
+const REDIRECT_URI ='https%3A%2F%2Fauth.expo.io%2Fgameplay'
+const SCOPE ='identify%20email%20connections%20guilds';
+const RESPONSE_TYPE ='token'
+const CLIENT_ID = '1034913782490747000';
 const CDN_IMAGE ='https://cdn.discordapp.com'
+
+//https://discord.com/api
+
+
