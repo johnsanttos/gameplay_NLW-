aaed693926fe28665d281d35f94c83efdbfcde74
John Santos <jhow.mito11@gmail.com>
2022-10-28 16:04:47 -0300

    Login discord

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -3,12 +3,12 @@ import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular,Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium , Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
-import { StatusBar} from 'react-native';
+import { StatusBar, LogBox} from 'react-native';
 import { Routes } from './src/routes';
-import { Home } from './src/screens/Home';
 import { Background } from './src/components/Background';
-import {SignIn} from './src/screens/SignIn';
 import { AuthProvider } from './src/hooks/auth';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.', 'expo-app-loading is deprecated in favor of expo-splash-screen:'])
 
 export default function App() {
   const [fontLoaded] = useFonts ({
--- gameplay/src/screens/SignIn/index.tsx
+++ gameplay/src/screens/SignIn/index.tsx
@@ -1,21 +1,22 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import { View, Text, Image, Alert } from "react-native";
+import { View, Text, Image, Alert, ActivityIndicator } from "react-native";
 
 import { useAuth } from "../../hooks/auth";
 import IllustrationImg from "../../assets/illustration.png";
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { styles } from "./styles";
 import { Background } from "../../components/Background";
+import { theme } from "../../global/styles/theme";
 
 export function SignIn() {
-  const { user, signIn } = useAuth();
+  const { loading, signIn } = useAuth();
 
   async function handleSignIn() {
     try {
       await signIn();
     } catch (error) {
-       Alert.alert('error') 
+      Alert.alert('error')
     }
   }
   return (
@@ -37,9 +38,14 @@ export function SignIn() {
             Crie grupos para jogar seus games{`\n`}
             favoritos com seus amigos
           </Text>
-
-          <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
-        </View>
+          
+          {
+            loading ?
+             <ActivityIndicator color={theme.colors.primary}/> 
+            :
+              <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} /> 
+          }
+            </View>
       </View>
     </Background>
   );
