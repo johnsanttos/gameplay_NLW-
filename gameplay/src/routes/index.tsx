31843d43aeee47f4ff1fefea7fbc9162cfc44c54
John Santos <jhow.mito11@gmail.com>
2022-10-31 10:44:54 -0300

    update

--- gameplay/src/routes/index.tsx
+++ gameplay/src/routes/index.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
 
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { AuthRoutes } from './auth.routes';
 import { useAuth } from '../hooks/auth';
+
 import { SignIn } from '../screens/SignIn';
+import { AuthRoutes } from './auth.routes';
+import { createStackNavigator } from '@react-navigation/stack';
+import { theme } from '../global/styles/theme';
 
-export function Routes () {
+export function Routes() {
+    const { user } = useAuth();
+    const { Navigator, Screen } = createStackNavigator();
 
-    const {user} = useAuth()
-
-    return(
+    return (
         <NavigationContainer>
-           {user.id? <AuthRoutes/> : <SignIn/>}
+            {user.id
+                ?
+                <AuthRoutes />
+                :
+                <Navigator
+                    headerMode="none"
+                    screenOptions={{
+                        cardStyle: {
+                            backgroundColor: theme.colors.secondary100
+                        }
+                    }}
+                >
+                    <Screen name="SignIn" component={SignIn} />
+                </Navigator>
+            }
         </NavigationContainer>
     )
 }
